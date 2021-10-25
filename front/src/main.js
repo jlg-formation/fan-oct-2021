03e2c97d3729a01cfdc88698e6e04b36cc9eda30
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-10-25 15:33:51 +0200

    dev: added layout components

--- front/src/main.js
+++ front/src/main.js
@@ -6,4 +6,10 @@
   module.component("appHeader", {
     templateUrl: "tmpl/appHeader.html",
   });
+  module.component("appBody", {
+    templateUrl: "tmpl/appBody.html",
+  });
+  module.component("appFooter", {
+    templateUrl: "tmpl/appFooter.html",
+  });
 })();
