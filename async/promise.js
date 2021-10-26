b7a5aa9b20e0e0ee8680e329417319540ad70a00
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-10-26 17:08:12 +0200

    lint: avoid small error

--- async/promise.js
+++ async/promise.js
@@ -16,8 +16,7 @@ const readFile = (filename, options) => {
   return new Promise((resolve, reject) => {
     fs.readFile(filename, options, (err, result) => {
       if (err) {
-        reject(err);
-        return;
+        return reject(err);
       }
       resolve(result);
     });
