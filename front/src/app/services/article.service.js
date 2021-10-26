340c0fc458b41a18562e04be85fc566707079f4d
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-10-26 17:06:04 +0200

    dev: start to converse with backend

--- front/src/app/services/article.service.js
+++ front/src/app/services/article.service.js
@@ -1,23 +1,39 @@
 var module = angular.module("app");
 
-module.service("articleService", function ArticleService() {
-  this.articles = [
-    { name: "Tournevis", price: 2.99, qty: 110 },
-    { name: "Pelle", price: 3.5, qty: 20 },
-    { name: "Pince", price: 4.0, qty: 30 },
-    { name: "Tondeuse à gazon", price: 200, qty: 4 },
-    { name: "Marteau", price: 1.23, qty: 200 },
-  ];
+module.service("articleService", [
+  "$http",
+  function ArticleService($http) {
+    this.articles = [
+      { name: "Tournevis", price: 2.99, qty: 110 },
+      { name: "Pelle", price: 3.5, qty: 20 },
+      { name: "Pince", price: 4.0, qty: 30 },
+      { name: "Tondeuse à gazon", price: 200, qty: 4 },
+      { name: "Marteau", price: 1.23, qty: 200 },
+    ];
 
-  this.add = function (article) {
-    this.articles.push(article);
-    this.$http
-      .post("/api/articles", article)
-      .then(() => {
-        this.refresh();
-      })
-      .catch((err) => {
-        console.log("err: ", err);
-      });
-  };
-});
+    this.add = function (article) {
+      this.articles.push(article);
+      $http
+        .post("/api/articles", article)
+        .then(() => {
+          this.refresh();
+        })
+        .catch((err) => {
+          console.log("err: ", err);
+        });
+    };
+
+    this.refresh = function () {
+      $http
+        .get("/api/articles")
+        .then((articles) => {
+          this.articles = articles;
+        })
+        .catch((err) => {
+          console.log("err: ", err);
+        });
+    };
+
+    this.refresh();
+  },
+]);
