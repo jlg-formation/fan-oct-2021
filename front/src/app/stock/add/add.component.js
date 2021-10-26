47d88f68b8d97a7fd4533a103f5d8865ea72acd8
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-10-26 16:02:15 +0200

    dev: added submit function to the form

--- front/src/app/stock/add/add.component.js
+++ front/src/app/stock/add/add.component.js
@@ -4,4 +4,12 @@ import "./add.component.scss";
 var module = angular.module("app.stock");
 module.component("appAdd", {
   template: html,
+  controller: [
+    "articleService",
+    function (articleService) {
+      this.submit = function () {
+        console.log("submit");
+      };
+    },
+  ],
 });
