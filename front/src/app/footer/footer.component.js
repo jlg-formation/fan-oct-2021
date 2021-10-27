c6ba49e41c06f92be671dd224e22ee1e185d8dc8
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-10-27 16:32:14 +0200

    dev: scope demo

--- front/src/app/footer/footer.component.js
+++ front/src/app/footer/footer.component.js
@@ -4,4 +4,13 @@ import "./footer.component.scss";
 const module = angular.module("app");
 module.component("appFooter", {
   template: html,
+  controller: class AppFooterCtrl {
+    constructor($scope) {
+      // example using scope. Just for demo.
+      // Please do not use scope. Prefer exposing attribute/method directly under the ctrl.
+      console.log("$scope: ", $scope);
+      const year = new Date().getFullYear();
+      $scope.copyright = `©${year} - Orsys`;
+    }
+  },
 });
