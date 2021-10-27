835bbee4e2575485280d907f128693b07cd3e380
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-10-27 17:01:35 +0200

    test: test add and remove article

--- front/cypress/integration/2-articles/articles.spec.js
+++ front/cypress/integration/2-articles/articles.spec.js
@@ -1,36 +1,24 @@
-describe('first test', () => {
-  it('should add an article', () => {
-    cy.intercept('GET', '/api/auth/isConnected').as('isConnected');
-    cy.visit('http://localhost:4200');
-    cy.wait('@isConnected');
-    cy.get('@isConnected').its('response.statusCode').should('eq', 401);
+describe("first test", () => {
+  it("should add an article", () => {
+    cy.visit("http://localhost:9000");
 
     const uuid = () => Cypress._.random(0, 1e6);
     const testname = `Testname${uuid()}`;
 
-    cy.get('header').contains('Se connecter');
-    cy.get('main').contains('Voir le stock').click();
-    cy.contains('Connexion avec Test Provider', { timeout: 10000 })
-      .should('be.visible')
-      .click();
+    cy.get("main").contains("Voir le stock").click();
 
-    cy.wait('@isConnected');
-    cy.get('@isConnected.2')
-      .its('response.statusCode')
-      .should('be.oneOf', [200, 304]);
+    cy.log("should show the stocks");
+    cy.get("main").contains("Ajouter").click();
+    cy.get("main").get("input[name=name]").clear().type(testname);
+    cy.get("main").get("input[name=price]").clear().type("1.45");
+    cy.get("main").get("input[name=qty]").clear().type("345");
 
-    cy.log('should show the stocks');
-    cy.get('main').contains('Ajouter').click();
-    cy.get('main').get('input[formcontrolname=name]').clear().type(testname);
-    cy.get('main').get('input[formcontrolname=price]').clear().type('1.45');
-    cy.get('main').get('input[formcontrolname=qty]').clear().type('345');
+    const getArticleAlias = "getArticles";
+    cy.intercept("GET", "/articles").as(getArticleAlias);
+    cy.get("main").get("button").contains("Ajouter").click();
+    cy.wait("@" + getArticleAlias);
 
-    const getArticleAlias = 'getArticles';
-    cy.intercept('GET', '/api/articles').as(getArticleAlias);
-    cy.get('main').get('button').contains('Ajouter').click();
-    cy.wait('@' + getArticleAlias);
-
-    cy.get('main').contains(testname).click();
-    cy.get('main').contains('Supprimer').click();
+    cy.get("main").contains(testname).click();
+    cy.get("main").contains("Supprimer").click();
   });
 });
