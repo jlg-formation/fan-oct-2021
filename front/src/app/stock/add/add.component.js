b83bab2b9204543dc33d85bfe06c367cfed79ff8
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-10-27 14:58:20 +0200

    refactor: use ng annotate for class

--- front/src/app/stock/add/add.component.js
+++ front/src/app/stock/add/add.component.js
@@ -4,24 +4,20 @@ import "./add.component.scss";
 const module = angular.module("app.stock");
 module.component("appAdd", {
   template: html,
-  controller: [
-    "articleService",
-    "$state",
-    class AppAddCtrl {
-      constructor(articleService, $state) {
-        this.article = {
-          name: "Truc",
-          price: 1.23,
-          qty: 100,
-        };
-        this.articleService = articleService;
-        this.$state = $state;
-      }
+  controller: class AppAddCtrl {
+    constructor(articleService, $state) {
+      this.article = {
+        name: "Truc",
+        price: 1.23,
+        qty: 100,
+      };
+      this.articleService = articleService;
+      this.$state = $state;
+    }
 
-      submit() {
-        this.articleService.add(this.article);
-        this.$state.go("stockList");
-      }
-    },
-  ],
+    submit() {
+      this.articleService.add(this.article);
+      this.$state.go("stockList");
+    }
+  },
 });
