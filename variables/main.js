ac4e08aed6133d713573853ef8d2de369b356736
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-10-27 14:41:40 +0200

    dev: javascript syntax

--- variables/main.js
+++ variables/main.js
@@ -1,10 +1,34 @@
-console.log("a: ", a);
-const a = {};
-console.log("a: ", a);
+"use strict";
 
-a.titi = 4;
-toto();
+var maJolieVariable = 32;
 
-function toto() {
+maJolieVariable = 45;
+
+console.log("this: ", this);
+
+function toto(qq, aa) {
   console.log("toto");
+  console.log("qq: ", qq);
+  console.log("aa: ", aa);
+  console.log("this: ", this);
 }
+
+const titi = () => {
+  console.log("toto");
+  console.log("this: ", this);
+};
+
+const truc = {
+  tata: toto,
+};
+
+toto("hello", "string");
+titi();
+truc.tata("hello", "string");
+
+toto.bind(34)("hello", "string");
+toto.apply(12, ["hello", "string"]);
+toto.call(13, "hello", "string");
+
+const zz = new toto("hello", "string");
+console.log("zz: ", zz);
